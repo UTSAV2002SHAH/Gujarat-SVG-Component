c8a9d092de9f56da2f5a6ac2de759edfd26ebbdc
UTSAV2002SHAH <utsavshah172002@gmail.com>
2025-05-08 12:38:52 +0530

    added deployment code

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/Gujarat-SVG-Component',
+  trailingSlash: true,
 };
 
 export default nextConfig;
--- src/app/profile/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Profilepage(){
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>Profile</h1>
-            <hr className="text-black"/>
-            <p>Profile Page</p>
-        </div>
-    )
-}
