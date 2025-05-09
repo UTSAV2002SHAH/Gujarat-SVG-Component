48cb0592babc5b20d2ee1ca395ea4dbac2e40149
UTSAV2002SHAH <utsavshah172002@gmail.com>
2025-05-09 14:30:28 +0530

    next-config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/Gujarat-SVG-Component',
-  assetPrefix: '/Gujarat-SVG-Component',
+  assetPrefix: '/Gujarat-SVG-Component/',
   trailingSlash: true,
 };
 
